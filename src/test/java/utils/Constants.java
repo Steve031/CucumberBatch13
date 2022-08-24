6f26f4b11035355e52fa47ba924bcdd4fb876e79
stave031 <stevealba7382@gmail.com>
2022-08-23 22:52:31 -0400

    cucumber classes

---
+++ src/test/java/utils/Constants.java
@@ -0,0 +1,12 @@
+package utils;
+
+public class Constants {
+
+    public static final String CONFIGURATION_FILEPATH ="/src/test/resources/config/config.properties";
+    public static final int IMPLICIT_WAIT = 10;
+    public static final int EXPLICIT_WAIT = 20;
+    public static final  String TEST_DATA_FILEPATH ="/src/test/resources/testData/testDataCucumber.xlsx";
+    public static final String SCREENSHOT_FILEPATH ="/screenshots/";
+
+}
+/Users/steve/IdeaProjects/CucumberBatch13/src/test/java/pages
